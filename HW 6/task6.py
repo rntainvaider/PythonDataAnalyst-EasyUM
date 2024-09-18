c27c50bf109d8c5d9d3125e34dadea13833499b0
evgenii <vovk.evgenii.199811@gmail.com>
2024-09-18 16:46:35 +0300

    add file in folder HW 6

---
+++ HW 6/task6.py
@@ -0,0 +1,38 @@
+def get_list_assessments(count_assessments: int):
+    list_assessments = list()
+    for _ in range(1, count_assessments + 1):
+        numb = int(input("Введите оценку: "))
+        if 3 <= numb <= 5:
+            list_assessments.append(numb)
+        else:
+            print("Оценка должна быть 3, 4 или 5!!!")
+    return list_assessments
+
+def count_assessments_in_list(list_assessments):
+    count_fhree = 0
+    count_four = 0
+    count_five = 0
+    
+    for fhree in list_assessments:
+        if fhree == 3:
+            count_fhree += 1
+
+    for four in list_assessments:
+        if four == 4:
+            count_four += 1
+
+    for five in list_assessments:
+        if five == 5:
+            count_five += 1
+
+    return count_fhree
+
+count_assessments_in_list(get_list_assessments())
+
+while True:
+    count_assessments = int(input("Введите количество оценок: "))
+    if count_assessments < 0:
+        print("Отрицательным не может быть!!!")
+    else:
+        print(get_list_assessments(count_assessments))
+        break
